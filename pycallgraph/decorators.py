dcbede9b8e1a799399f5f96ce258dffd90a08db2
Peter Schmidt <pschmidt@mathspace.com.au>
2013-10-30 18:26:30 +1100

    Add more docstring and TODO.

--- pycallgraph/decorators.py
+++ pycallgraph/decorators.py
@@ -7,8 +7,13 @@ def pycallgraphthis(outfile='pycallgraph.png', tool='dot'):
     Just drop the decorator on any function you'd like to profile and see a
     call graph of.
 
+        @pycallgraphthis(outfile='pycallgraph.png')
+        def my_method():
+            ...
+
     Note that it may take a short while to build the graph.
     """
+    # TODO: Fix bug where basic @pycallgraphthis with no args doesn't work
     def wrap(f):
         def wrapped_f(*args, **kwargs):
             output = GraphvizOutput(
